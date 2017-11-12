42676c3899cb80f3d788a5439c3ae403da6c96d8
Andrew Burrows <burrowsa@gmail.com>
2017-11-12 21:10:04 +0000

    Solving one more to make it 25 and unlock level 1

---
+++ ProjectEuler/problem22.h
@@ -0,0 +1,3 @@
+#pragma once
+
+unsigned problem22() noexcept;
---
+++ ProjectEuler/test_problem22.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "problem22.h"
+
+namespace
+{
+	TEST(Problem22Tests, TestProblem22)
+	{
+		EXPECT_EQ(871198282u, problem22());
+	}
+}
